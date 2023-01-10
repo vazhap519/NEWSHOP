bc1c037f2afc59da64766efafc0d7d8e7c48b468
vazha <phankvelashvili.mail@gmail.com>
2023-01-10 16:35:16 +0400

    add langs

--- config/translation.php
+++ config/translation.php
@@ -11,7 +11,7 @@ return [
     | Supported: "file", "database"
     |
     */
-    'driver' => 'database',
+    'driver' => 'database','file',
 
     /*
     |--------------------------------------------------------------------------
